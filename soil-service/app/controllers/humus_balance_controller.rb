293d29379defadfafc63a3d7cd4f1e337845f89d
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-07-28 20:57:19 +0200

    Configure humus_balance route and include HumusBalanceHelper into humus controller

--- soil-service/app/controllers/humus_balance_controller.rb
+++ soil-service/app/controllers/humus_balance_controller.rb
@@ -1,9 +1,13 @@
 class HumusBalanceController < ApplicationController
+    skip_before_action :verify_authenticity_token
 
-    def get_balances
+    include HumusBalanceHelper
+
+    def create
         fields_data = params[:_json]
+        
         return unless fields_data.present?
-    
+        
         humus_balance = fields_data.map { |field| { field_id: field[:id], humus_balance: calc_balance(field[:crops])}}
         render json: humus_balance
       end
