581f6867f282db2ea2486b08ed6c8cc8c654a19d
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-07-29 01:06:51 +0200

    Test if post humus request is valid or not valid

--- soil-service/spec/requests/humus_balance_spec.rb
+++ soil-service/spec/requests/humus_balance_spec.rb
@@ -1,7 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe "HumusBalances", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'HumusBalances API', type: :request do
+
+  describe 'POST /humus_balance' do
+    
+    let(:body) { FieldsService.instance.fetch_fields.to_json }
+    let(:header) {{ 'Content-Type' => 'application/json' }}
+
+    context 'when request attributes are valid' do
+      before { post "/humus_balance", params: body, headers: header }
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when request attributes are not valid' do
+      before { post "/humus_balance", params: body }
+
+      it 'returns status code 204' do
+        expect(response).to have_http_status(204)
+      end
+    end
   end
 end
